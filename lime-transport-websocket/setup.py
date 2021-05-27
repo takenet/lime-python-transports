2a043aafbc347f2f1dd63cd33025c04129e6ce14
Gabriel Santos <gabrielrsantoss@icloud.com>
2021-05-27 15:24:41 -0300

    Fix: license

--- lime-transport-websocket/setup.py
+++ lime-transport-websocket/setup.py
@@ -34,7 +34,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
